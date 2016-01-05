33fc6733abdea306d9c6615cbfa47c6a8158294b
Takuya Iwanaga <takuyai@gmail.com>
2016-01-05 17:47:31 +1100

    Updated work on ecology
    Started work on implementing the ecology model as a submodule of the
    integrated model

--- Modules/Ecology/FlowSuitability.py
+++ Modules/Ecology/FlowSuitability.py
@@ -8,39 +8,64 @@ class FlowSuitability(object):
         """
         Create a list of flood events based on daily flow, like hydromad eventseq
         Output attributes:
-        event id, start date, end date, duration, month of start date, interflood dry period
+        event id, start date, end date, duration, startmonth (month when the event start), dry (interflood dry period)
         """
-        return flood_events
+        
+        return flood_events_df
     
-    def FloodIndexes(self, flood_events):
+    def FloodIndexes(self, flood_events_df, coordinates):
         """
         Calculate index for each flood attribute based on index curve (defined in WaterSuitability.py)
+        input: coordinates is a dict of duration, timing, dry coordinates
         Output attributes:
-        event id, start date, end date, duration index, timing index, interflood dry period index
+        event id, start date, end date, duration, startmonth (month when the event start), dry (interflood dry period),duration index, timing index, interflood dry period index
         """
-        return flood_indexes
+
+        duration = coordinates["duration"]
+        timing = coordinates["timing"]
+        dry = coordinates["dry"]
+        
+        #flood_indexes = flood_events_df.copy()
+        flood_events_df['duration_index'] = np.interp(flood_events_df.duration, duration.x, duration.y)
+        flood_events_df['timing_index'] = np.interp(flood_events_df.startmonth, timing.x, timing.y)
+        flood_events_df['dry_index'] = np.interp(flood_events_df.dry, dry.x, dry.y)
+        
+        return flood_events_df
     
     
-    def EventIndexWeightedAvg(self,flood_indexes):
+    def EventIndexWeightedAvg(self,flood_events_df, weights):
         """
         Calculate flood index summary based on weighted average for each event
         Output attributes:
         event id, start date, end date, summary flood index
         """
-        return event_index
+        flood_events_df['flood_index_weightavg']= flood_events_df['duration_index']*weights['duration']+flood_events_df['timing_index']*weights['timing']+flood_events_df['dry_index']*weights['dry']
+        return flood_events_df
         
-    def EventIndexMin(self,flood_indexes):
+    def EventIndexMin(self,flood_events_df):
         """
         Calculate flood index summary based on min rule
         Output attributes:
         event id, start date, end date, summary flood index
         """
-        return event_index   
+        flood_events_df['flood_index_min'] = flood_indexes.apply(lambda x: min(x["duration_index"], x["timing_index"], x["dry_index"]), axis=1)
         
-    def FlowIndex(self,event_index):
+        return flood_events_df   
+        
+        
+    def FlowIndex(self, flow_df, flood_events_df, aggregate_option):
         """
         Calculate daily flow index based on event index
         Output attributes:
         Date, flow_index
         """
+
+        flow_index = pd.DataFrame(flow_df["Flow"], index=flow_df.index)
+        flow_index["Flow"] = 0
+        flow_index.columns = ["flow_index"]
+        
+        flow_index["flow_index"] = flood_events_df[(flow_index.index >= flood_events_df["start_date"]) & (flow_index.index <= flood_events_df["end_date"])][aggregate_option]
+        
+        #flood_events_df['flood_index_sum']
+        
         return flow_index
--- Modules/Ecology/WaterSuitability.py
+++ Modules/Ecology/WaterSuitability.py
@@ -23,39 +23,66 @@ class WaterSuitability(object):
         
         #End init()
 
-    def selectCoordinates(self,curve_option):
+    def selectCoordinates(self, index_input, curve_option):
         """
-        Select a set of xy coordinates (imported from csv file) for generating curve.
+        Select a set of xy coordinates (index_curve is a data imported from csv file, coded in core) for generating curve.
         To be used for uncertainty analysis through selecting different curves
+        inputs: a curve csv file (for one species and one attribute), select one curve (column2-x) from that file
+        outputs: coordinates.x, coordinates.y
         """
-        return coordinates
 
-    def selectWeights(self,weight_option):
+        #Create 2 column dataframe of Days and desired column
+        temp_df = pd.DataFrame(index_input[curve_option], index=index_input["Days"])
+        temp_df = temp_df.dropna()
+        
+        #Recreate Days column from index
+        temp_df["x"] = temp_df.index
+        
+        temp_df.columns = ["x", "y"]
+        
+        return temp_df
+        
+        #coordinates['x']= index_input['x']
+        #coordinates['y']= index_input['curve_option']
+        #return coordinates
+
+    def selectWeights(self, weight_input, weight_option):
         """
         Select a set of weight (imported from csv file) for aggregating indexes.
         To be used for uncertainty analysis through selecting different weights
+        outputs: duration_weights, timing_weights, dry_weights
         """
+        
+        weights = {}
+        weights['duration'] = weight_input[weight_input['Attribute'] == 'Duration'][weight_option]
+        weights['timing'] = weight_input[weight_input['Attribute'] == 'Timing'][weight_option]
+        weights['dry'] = weight_input[weight_input['Attribute'] == 'Dry'][weight_option]
         return weights            
         
-    def modifyCoordinates(self, coordinates):
+    def modifyCoordinates(self, coordinates,adjust):
         """
         Adjust the predefined xy coordinates e.g. +_ 10%
         To be used for uncertainty analysis through allowing the predefined xy coordinates to move around
         """
+        new_coordinates['x']= coordinates['x']*adjust
+        new_coordiantes['y']= coordinates['y']*adjust
         return new_coordinates
 
 
-    def makeIndexCurve(self, coordinates):
-        """
-        Create an index curve based on xy coordinates. 
-        The two ends of the curve extend flat (i.e. the same as the first/last y value)
-        """
-        return index_curve
+#    def makeIndexCurve(self, coordinates):
+#        """
+#        Create an index curve based on xy coordinates. 
+#        The two ends of the curve extend flat (i.e. the same as the first/last y value)
+#        """
+#        return index_curve
     
-    def WaterIndexWeightedAvg(self,flow_index, groundwater_index):
+    def WaterIndexWeightedAvg(self,flow_index, groundwater_index, groundwater_weight):
         """
         Calculate daily water index summary based on weighted average of daily flow index and groundwater index
         Output attributes:
-        Date, water_index
+        Date, water_index  
         """
+        flow_index.merge(groundwater_index, left_index=True)
+        
+        water_index = flow_index * (1-groundwater_weight) + groundwater_index * groundwater_weight
         return water_index
