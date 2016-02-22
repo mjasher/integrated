e101c3d9015a2dd762f12167b1df3152a578fef7
Takuya Iwanaga <takuyai@gmail.com>
2016-02-22 11:21:16 +1100

    Changes for sphinxdoc
    Wrapped loose scripts with name == main check so that sphinxdoc doesn't
    run the file

--- Modules/Ecology/flow_dev.py
+++ Modules/Ecology/flow_dev.py
@@ -1,126 +1,128 @@
 #flow_dev.py
 
-#import os
-#os.chdir('C:\\UserData\\fub\\work09\\MDB')
-from integrated.Modules.Ecology.FlowSuitability import FlowSuitability
+if __name__ == '__main__':
 
-from integrated.Modules.Core.Handlers.FileHandler import FileHandler
+    #import os
+    #os.chdir('C:\\UserData\\fub\\work09\\MDB')
+    from integrated.Modules.Ecology.FlowSuitability import FlowSuitability
 
-FileHandle = FileHandler()
+    from integrated.Modules.Core.Handlers.FileHandler import FileHandler
 
-#Paths to files
-dev_data_path = "Integrated/Modules/Ecology/Inputs"
-#dev_data_path = "Inputs"
+    FileHandle = FileHandler()
 
-# Read in flow data
-scenarios = [dev_data_path+"/Hydrology/sce1", dev_data_path+"/Hydrology/sce2"]
+    #Paths to files
+    dev_data_path = "Integrated/Modules/Ecology/Inputs"
+    #dev_data_path = "Inputs"
 
-date_range = ["1900-01-01", None]
+    # Read in flow data
+    scenarios = [dev_data_path+"/Hydrology/sce1", dev_data_path+"/Hydrology/sce2"]
 
-# Read in index data
-# NOTE: Left most column will be used as the DataFrame index
-indexes = FileHandle.importFiles(dev_data_path+"/Ecology/index", ext=".csv", index_col=0, walk=False)
-indexes = indexes["index"] #Remove parent folder listing from Dict, as this is unneeded
+    date_range = ["1900-01-01", None]
 
-# read in asset table
-#This could be set as class attribute as it is used as a global in the R script
-asset_table = FileHandle.loadCSV(dev_data_path+"/Ecology/Water_suitability_param.csv")
+    # Read in index data
+    # NOTE: Left most column will be used as the DataFrame index
+    indexes = FileHandle.importFiles(dev_data_path+"/Ecology/index", ext=".csv", index_col=0, walk=False)
+    indexes = indexes["index"] #Remove parent folder listing from Dict, as this is unneeded
 
-# read in weights
-weights = indexes["weights"]
+    # read in asset table
+    #This could be set as class attribute as it is used as a global in the R script
+    asset_table = FileHandle.loadCSV(dev_data_path+"/Ecology/Water_suitability_param.csv")
 
-#change headers to lowercase to make it consistent with other csvs
-weights.columns = [x.lower() for x in weights.columns]
+    # read in weights
+    weights = indexes["weights"]
 
-# Set up additional parameters:
-# Set up weight for groundwater index
-gw_weight = 0.2
+    #change headers to lowercase to make it consistent with other csvs
+    weights.columns = [x.lower() for x in weights.columns]
 
-# For DSS, can use RRGMS only as a minimum.
-specieslist = ["RRGMS", "RRGRR"]
+    # Set up additional parameters:
+    # Set up weight for groundwater index
+    gw_weight = 0.2
 
-FlowIndexer = FlowSuitability(asset_table, specieslist, indexes, weights)
+    # For DSS, can use RRGMS only as a minimum.
+    specieslist = ["RRGMS", "RRGRR"]
 
-#Generate default index column choice for each species, and each variable
-#Stores result as an attribute (default_index_cols)
-FlowIndexer.generateDefaultIndexCols()
+    FlowIndexer = FlowSuitability(asset_table, specieslist, indexes, weights)
 
-#Could specify this and pass to generateEnvIndex() as the species_col parameter
-#species_col should look like:
-# {
-# 	'RRGMS': {
-# 		'timing': 'MFAT1', 
-# 		'duration': 'MFAT1', 
-# 		'dry_period': 'MFAT1', 
-# 		'gwlevel': 'Index', 
-# 		'salinity': 'Index'
-# }, 'RRGRR': {
-# 		'timing': 'MFAT1', 
-# 		'duration': 'MFAT1', 
-# 		'dry_period': 'Index', 
-# 		'gwlevel': 'Index', 
-# 		'salinity': 'Index'
-# 	}
-# }
+    #Generate default index column choice for each species, and each variable
+    #Stores result as an attribute (default_index_cols)
+    FlowIndexer.generateDefaultIndexCols()
 
-species_cols = {
- 	'RRGMS': {
- 		'timing': 'MFATwoodland', 
- 		'duration': 'MFATwoodland', 
- 		'dry_period': 'Roberts', 
- 		'gwlevel': 'Index', 
- 		'salinity': 'Index'
- }, 'RRGRR': {
- 		'timing': 'Roberts', 
- 		'duration': 'Roberts', 
- 		'dry_period': 'Index', 
- 		'gwlevel': 'Index', 
- 		'salinity': 'Index'
- 	}
-}
+    #Could specify this and pass to generateEnvIndex() as the species_col parameter
+    #species_col should look like:
+    # {
+    #   'RRGMS': {
+    #       'timing': 'MFAT1', 
+    #       'duration': 'MFAT1', 
+    #       'dry_period': 'MFAT1', 
+    #       'gwlevel': 'Index', 
+    #       'salinity': 'Index'
+    # }, 'RRGRR': {
+    #       'timing': 'MFAT1', 
+    #       'duration': 'MFAT1', 
+    #       'dry_period': 'Index', 
+    #       'gwlevel': 'Index', 
+    #       'salinity': 'Index'
+    #   }
+    # }
 
-#This is unused at the moment
-# weighted = lambda d, gweight: d[1]*gweight + d[2]*(1-gweight)
+    species_cols = {
+        'RRGMS': {
+            'timing': 'MFATwoodland', 
+            'duration': 'MFATwoodland', 
+            'dry_period': 'Roberts', 
+            'gwlevel': 'Index', 
+            'salinity': 'Index'
+     }, 'RRGRR': {
+            'timing': 'Roberts', 
+            'duration': 'Roberts', 
+            'dry_period': 'Index', 
+            'gwlevel': 'Index', 
+            'salinity': 'Index'
+        }
+    }
 
-for scenario_dir in xrange(0, len(scenarios)):
+    #This is unused at the moment
+    # weighted = lambda d, gweight: d[1]*gweight + d[2]*(1-gweight)
 
-	scen = scenarios[scenario_dir]
+    for scenario_dir in xrange(0, len(scenarios)):
 
-	scen_name = scen.split('/')[-1]
+        scen = scenarios[scenario_dir]
 
-	scenario_data = FileHandle.importFiles(scen, index_col="Date", parse_dates=True, date_range=date_range, dayfirst=True) #read in all gauges within each scenarios
+        scen_name = scen.split('/')[-1]
 
-	#For each asset, generate flow indexes for each species
-	for j in xrange(0, len(asset_table.index)):
+        scenario_data = FileHandle.importFiles(scen, index_col="Date", parse_dates=True, date_range=date_range, dayfirst=True) #read in all gauges within each scenarios
 
-		flow_indexes = FlowIndexer.generateEnvIndex(asset_id=j, scen_data=scenario_data, ecospecies=specieslist, species_cols=species_cols) #species_cols=FlowIndexer.default_index_cols, gswfun=weighted
+        #For each asset, generate flow indexes for each species
+        for j in xrange(0, len(asset_table.index)):
 
-		#For development purposes only. Display index results for each species
-		print "Asset {id}".format(id=j+1)
-		for species in specieslist:
+            flow_indexes = FlowIndexer.generateEnvIndex(asset_id=j, scen_data=scenario_data, ecospecies=specieslist, species_cols=species_cols) #species_cols=FlowIndexer.default_index_cols, gswfun=weighted
 
-			save_folder = "./Integrated/Modules/Ecology/Outputs/{s}/asset_{asset}/{sp}".format(s=scen_name, asset=j+1, sp=species)
+            #For development purposes only. Display index results for each species
+            print "Asset {id}".format(id=j+1)
+            for species in specieslist:
 
-			species_index = flow_indexes[species]
+                save_folder = "./Integrated/Modules/Ecology/Outputs/{s}/asset_{asset}/{sp}".format(s=scen_name, asset=j+1, sp=species)
 
-			print species
-			print "---- Groundwater ----"
-			FileHandle.writeCSV(species_index['gw'], save_folder, "gw.csv")
-			# print species_index['gw']
-			print "---------------------"
-			print "---- Surface Water ----"
-			FileHandle.writeCSV(species_index['sw'], save_folder, "sw.csv")
-			# print species_index['sw']
-			print "---------------------"
-			print "---- Water Index ----"
-			FileHandle.writeCSV(species_index['water_index'], save_folder, "water_index.csv")
-			# print species_index['water_index']
-			print "---------------------"
-		#End for
+                species_index = flow_indexes[species]
 
-		#Save indexes to file or something?
+                print species
+                print "---- Groundwater ----"
+                FileHandle.writeCSV(species_index['gw'], save_folder, "gw.csv")
+                # print species_index['gw']
+                print "---------------------"
+                print "---- Surface Water ----"
+                FileHandle.writeCSV(species_index['sw'], save_folder, "sw.csv")
+                # print species_index['sw']
+                print "---------------------"
+                print "---- Water Index ----"
+                FileHandle.writeCSV(species_index['water_index'], save_folder, "water_index.csv")
+                # print species_index['water_index']
+                print "---------------------"
+            #End for
 
-	#End for
+            #Save indexes to file or something?
 
-#End for
+        #End for
+
+    #End for
 
