d612465d82056f74937765db9c0d5bcb5ea97995
Takuya Iwanaga <takuyai@gmail.com>
2016-03-16 12:27:47 +1100

    Moved financial methods into Finance class

--- Modules/Farm/Farms/FarmComponent.py
+++ Modules/Farm/Farms/FarmComponent.py
@@ -27,22 +27,3 @@ class FarmComponent(Component):
 	def calcImplementationCostPerHa(self):
 		pass
 	#End calcImplementationCostPerHa()
-
-	def annualizeCapital(self, capital, discount_rate=0.07, num_years=20):
-
-	    """
-	    Calculates capital value per year over a given number of years
-
-	    :param capital: Amount of money in dollars ($)
-	    :param discount_rate: The percentage to discount future value to get present-day value; Arshad et al. (2013) used 7%
-	    :param num_years: Number of years for the scenario
-	    :returns: Annualised capital cost discounted to present day value 
-	    :return type: numeric
-	    
-	    """
-
-	    annuity_factor = (1 - 1/(1 + discount_rate)**num_years)/discount_rate
-
-	    return capital/annuity_factor
-
-	#End calcCapitalCostPerYear()
