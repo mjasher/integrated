00a5fca2f689a10aaffeb30d08993ffd611954fb
Takuya Iwanaga <takuyai@gmail.com>
2016-05-22 12:09:11 +1000

    Sensitivity Analysis package example
    Taken from the github page, just seeing what the output looks like

---
+++ examples/salib_example.py
@@ -0,0 +1,25 @@
+from SALib.sample import saltelli
+from SALib.analyze import sobol
+from SALib.test_functions import Ishigami
+import numpy as np
+
+problem = {
+  'num_vars': 3, 
+  'names': ['x1', 'x2', 'x3'], 
+  'bounds': [[-np.pi, np.pi]]*3
+}
+
+# Generate samples
+param_values = saltelli.sample(problem, 1000, calc_second_order=True)
+
+# Run model (example)
+Y = Ishigami.evaluate(param_values)
+
+# Perform analysis
+Si = sobol.analyze(problem, Y, print_to_console=False)
+# Returns a dictionary with keys 'S1', 'S1_conf', 'ST', and 'ST_conf'
+# (first and total-order indices with bootstrap confidence intervals)
+
+print Y
+
+print Si
